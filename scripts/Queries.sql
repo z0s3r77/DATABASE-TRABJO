b18720e79c018ab0a90208ecd5e244c8fb55a4c1
Sebastián Estacio R <80277545+z0s3r77@users.noreply.github.com>
2023-03-29 18:13:43 +0200

    Update Queries.sql

--- scripts/Queries.sql
+++ scripts/Queries.sql
@@ -1,10 +1,40 @@
 -- TIEMPO
 
+ SELECT Games.name  FROM Purchases
+ JOIN Games ON Purchases.game_id = Games.id
+ WHERE MONTH(Purchases.purchase_date) = 3;
+ 
++------------------------+
+| name                   |
++------------------------+
+| Super Mario Bros.      |
+| Resident Evil 4 remake |
++------------------------+
+2 rows in set (0.00 sec)
+
+
 -- INDEX
 
 -- INDEX 
 
 -- JOIN
+
+SELECT Developers.name, Developers.surname  FROM Developers  
+INNER JOIN GamesDevelopers ON Developers.id = GamesDevelopers.developer_id  
+INNER JOIN Games ON Games.id = GamesDevelopers.game_id  
+WHERE Games.name = 'Resident Evil 4 remake';
+
++-----------+---------+
+| name      | surname |
++-----------+---------+
+| John      | Smith   |
+| Jennifer  | Davis   |
+| Isaac     | Clarke  |
+| Manu      | Manolo  |
+| Sebastian | Estacio |
+| Amin      | Grande  |
++-----------+---------+
+6 rows in set (0.00 sec)
 
 -- JOIN
 
@@ -12,4 +42,15 @@
 
 -- MERGE
 
--- 
+-- ALL EARNINGS
+
+SELECT SUM(price * 0.9) as Earnings FROM Purchases;
++----------+
+| Earnings |
++----------+
+|  278.946 |
++----------+
+1 row in set (0.00 sec)
+
+
+
